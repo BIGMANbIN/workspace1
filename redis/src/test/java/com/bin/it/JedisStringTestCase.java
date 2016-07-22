bbfe17fa2bdc9b58b2649b9c63a15152a548de75
BIGMANbIN <HB_1026@163.com>
2016-07-22 08:37:51 +0800

    Redis

---
+++ redis/src/test/java/com/bin/it/JedisStringTestCase.java
@@ -0,0 +1,7 @@
+package com.bin.it;
+
+/**
+ * Created by Administrator on 2016/7/21 0021.
+ */
+public class JedisStringTestCase {
+}
---
+++ redis/src/test/java/com/bin/it/SpringRedisTestCase.java
@@ -0,0 +1,7 @@
+package com.bin.it;
+
+/**
+ * Created by Administrator on 2016/7/22 0022.
+ */
+public class SpringRedisTestCase {
+}
